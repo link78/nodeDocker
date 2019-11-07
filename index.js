41c83471594f3b288b598657679420108c101984
Abdul Kade <akade@kadelab.com>
2019-11-06 22:21:47 -0600

    updated the index.html file by adding /about and /secret

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-'use strict'
-
+'strict'
+const crypto = requie('crypto');
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -9,6 +9,14 @@ const HOST = '0.0.0.0';
 app.get('/', (req, res) => {
 	res.send('Hello Node Docker');
 });
+app.get('/about',(req,res) => {
+        res.send('This is BFK-Solutions that focus on training kids in special need');
+});
+const secret = 'kadederklin78';
+const hash = crypto.createHmac('sha256',secret).update('I love linuxacademy').digest('hex');
 
+app.get('/secret',(res,req) => {
+        res.send('Here is the secret: ' + hash);
+});
 app.listen(PORT,HOST);
 console.log(`Running on https://${PORT}:{host}`);
